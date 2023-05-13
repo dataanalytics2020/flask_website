2a77d5e5beb5f5f664fc1b15e4bec25476f2144b
slotdata2020 <slotdata2020@gmail.com>
2023-05-13 23:56:01 +0900

    修正

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-from flaskr import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
