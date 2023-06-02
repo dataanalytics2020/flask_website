319ef291034b801bb21e8cb09dda09020b4da3c8
slotdata2020 <slotdata2020@gmail.com>
2023-06-03 04:41:49 +0900

    余計なコメントを削除

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def test():
     print('test')
     return render_template('test.html')
 
-
-
 @app.route('/<prefecture>', methods=['GET', 'POST'])
 def test2(prefecture):
     tenpo_url_df:pd.DataFrame = pd.read_csv('csv/tenpo_url_flask_web_site.csv')
@@ -61,7 +59,6 @@ def test2(prefecture):
 
     sorted_group_name_count_dict = sorted(group_name_count_dict.items(), key = lambda fruit : fruit[1])
     #print(type(sorted_group_name_count_dict))
-    # <class 'list'>
     sorted_group_name_count_dict.reverse()
     sorted_group_name_count_dict = dict(sorted_group_name_count_dict)
     sorted_group_name_count_dict
@@ -129,15 +126,7 @@ def clicked_tenpo_name(prefecture,tenpo_name):
         for serch_date in target_day_list:
             search_url = url = f"https://ana-slo.com/{serch_date}-{user_data['tenpo-name']}-data/"
             urls.append(search_url)
-            #search_response = requests.get(search_url)
-            #print(search_response)
-            # soup = BeautifulSoup(search_response.text, 'html.parser')
-            # dfs = pd.read_html(str(soup))
-            # for df in dfs:
-            #     if '機種名' in list(df.columns):
-            #         tmp_df = df
-            #         tmp_df['店舗名'] = serch_tenpo_name
-            #         tmp_df['機種名'
+
         with ThreadPoolExecutor(serch_number) as executor:
             results = list(executor.map(requests.get, urls))
         print(results)
@@ -270,15 +259,7 @@ def target_date_analytics():
         for serch_date in target_day_list:
             search_url = url = f"https://ana-slo.com/{serch_date}-{user_data['tenpo-name']}-data/"
             urls.append(search_url)
-            #search_response = requests.get(search_url)
-            #print(search_response)
-            # soup = BeautifulSoup(search_response.text, 'html.parser')
-            # dfs = pd.read_html(str(soup))
-            # for df in dfs:
-            #     if '機種名' in list(df.columns):
-            #         tmp_df = df
-            #         tmp_df['店舗名'] = serch_tenpo_name
-            #         tmp_df['機種名'
+
         with ThreadPoolExecutor(3) as executor:
             results = list(executor.map(requests.get, urls))
         print(results)
@@ -353,68 +334,6 @@ def target_date_analytics():
     else:
         return render_template('index.html')
 
-    #return render_template('index.html')
-
-
-    # 
-    # target_number = str(target_n_day)
-    # print()
-    # target_day_list = []
-    # number = 0
-    # for i in range(int(n_times)):
-    #     while True:
-    #         print(number)
-    #         if target_number == str(date.today() - timedelta(days=number))[-1]:
-    #             target_day = date.today() - timedelta(days=number)
-    #             print('取得日',target_day)
-    #             target_day_str = target_day.strftime('%Y-%m-%d')
-    #             target_day_list.append(target_day_str)
-    #             number += 1
-    #             break
-    #         else: 
-    #             pass
-    #         number += 1
-    # target_day_list   
-    # concat_df_list = []
-    # urls = []
-    # for serch_date in target_day_list:
-    #     search_url = url = f"https://ana-slo.com/{serch_date}-{serch_tenpo_name}-data/"
-    #     urls.append(search_url)
-    #     #search_response = requests.get(search_url)
-    #     #print(search_response)
-    #     # soup = BeautifulSoup(search_response.text, 'html.parser')
-    #     # dfs = pd.read_html(str(soup))
-    #     # for df in dfs:
-    #     #     if '機種名' in list(df.columns):
-    #     #         tmp_df = df
-    #     #         tmp_df['店舗名'] = serch_tenpo_name
-    #     #         tmp_df['機種名'
-    # with ThreadPoolExecutor(3) as executor:
-    #     results = list(executor.map(requests.get, urls))
-    # print(results)
-
-    # concat_df_list = []
-    # for search_response,date in zip(results, target_day_list):
-    #     soup = BeautifulSoup(search_response.text, "lxml")
-    #     elem = soup.select('#all_data_block')
-    #     dfs = pd.read_html(str(elem))
-    #     for df in dfs:
-    #         if '機種名' in list(df.columns):
-    #             tmp_df = df
-    #             tmp_df['店舗名'] = serch_tenpo_name
-    #             tmp_df['日付'] = date
-    #             #tmp_df['機種名'] = tmp_df['機種名'].map(removal_text)
-    #             break
-    #     concat_df_list.append(df)
-
-    # concat_df = pd.concat(concat_df_list,axis=0)
-    # concat_df = concat_df.groupby(['日付','機種名']).mean().sort_values('差枚',ascending=False)
-    # concat_df = concat_df[['G数','差枚']]
-    # concat_df['差枚'] =concat_df['差枚'].astype(int)
-    # concat_df['G数'] =concat_df['G数'].astype(int)
-    # concat_df = concat_df.reset_index()
-    # concat_df_html = concat_df.to_html(index=False)
-    # return  concat_df_html
 
 if __name__ == '__main__':
     app.run(debug=True)
