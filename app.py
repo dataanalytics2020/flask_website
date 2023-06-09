c413a59eef146ca2d806457793c0dd8293c5bfff
slotdata2020 <slotdata2020@gmail.com>
2023-06-09 23:59:35 +0900

    画像合成関数追加

--- app.py
+++ app.py
@@ -12,6 +12,31 @@ from PIL  import ImageDraw , ImageFont , Image
 
 df  = pd.read_csv(r'csv\2022-12-09_touhou.csv')
 
+
+def get_concat_h_multi_resize(im_list, resample=Image.BICUBIC):
+    min_height = min(im.height for im in im_list)
+    im_list_resize = [im.resize((int(im.width * min_height / im.height), min_height),resample=resample)
+                    for im in im_list]
+    total_width = sum(im.width for im in im_list_resize)
+    dst = Image.new('RGB', (total_width, min_height))
+    pos_x = 0
+    for im in im_list_resize:
+        dst.paste(im, (pos_x, 0))
+        pos_x += im.width
+    return dst
+
+def get_concat_v_multi_resize(im_list, resample=Image.BICUBIC):
+    min_width = min(im.width for im in im_list)
+    im_list_resize = [im.resize((min_width, int(im.height * min_width / im.width)),resample=resample)
+                    for im in im_list]
+    total_height = sum(im.height for im in im_list_resize)
+    dst = Image.new('RGB', (min_width, total_height))
+    pos_y = 0
+    for im in im_list_resize:
+        dst.paste(im, (0, pos_y))
+        pos_y += im.height
+    return dst
+
 #テーブルタイプの画像出力
 def create_df_cell_image(_df,image_name):
     global create_df_cell_image_path
