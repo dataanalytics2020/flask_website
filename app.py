1c60dadc0d0ea02d547a5267d881d80018db75bc
slotdata2020 <slotdata2020@gmail.com>
2023-07-26 00:54:00 +0900

    本番用にdebugを変更

--- app.py
+++ app.py
@@ -732,4 +732,4 @@ def target_date_analytics():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0",debug=False, port=int(os.environ.get('PORT', 5000)))
