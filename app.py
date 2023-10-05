2599ad6d6068b18cc66723a064d7c87d890316de
slotdata2020 <slotdata2020@gmail.com>
2023-10-05 16:04:04 +0900

    デバッグFALSEに変更

--- app.py
+++ app.py
@@ -1472,4 +1472,4 @@ def sitemap():
     return app.send_static_file("sitemap.xml")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0",debug=False, port=int(os.environ.get('PORT', 5000)))
