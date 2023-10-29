d51ce77bcea8f547ae47b2703b08b79412fd8922
slotdata2020 <slotdata2020@gmail.com>
2023-10-30 05:08:16 +0900

    トップに戻るボタン追加

--- static/js/script.js
+++ static/js/script.js
@@ -37,3 +37,21 @@ $(function(){
     }
 });
 
+
+$(function() {
+    var topBtn = $('.page-top');    
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 50);
+        return false;
+    });
+});
